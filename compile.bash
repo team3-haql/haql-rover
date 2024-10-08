26a69c7e2925313ed279c27108afbe5c2f56855f
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2024-10-08 12:25:06 -0600

    Can compile in docker bash and in normal bash

--- compile.bash
+++ compile.bash
@@ -1,7 +1,23 @@
 #!/bin/bash
+# Run with: . compile.bash
+# NOTE: Make sure you are not bashed into docker when running this.
 
-cd /home/ws_dev
-source /opt/ros/humble/setup.bash
-colcon build --symlink-install
-echo "COMPILATION COMPLETE"
-cd /home/ws_dev/src/haql-rover
+if [ ! -f /home/setup ]; then
+
+docker-compose run -it ralphee bash -c "
+
+    cd /home/ws_dev
+    source /opt/ros/humble/setup.bash
+    colcon build --symlink-install
+    echo 'COMPILATION COMPLETE'
+    cd /home/ws_dev/src/haql-rover
+
+"
+
+else  
+    cd /home/ws_dev
+    source /opt/ros/humble/setup.bash
+    colcon build --symlink-install
+    echo 'COMPILATION COMPLETE'
+    cd /home/ws_dev/src/haql-rover
+fi
