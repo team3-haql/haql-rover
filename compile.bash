9b43080775c690c9dc4f323d6a8505a37d478e87
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2024-10-12 22:46:40 +0000

    Everything works except traverse layer

--- compile.bash
+++ compile.bash
@@ -22,7 +22,8 @@ else
     cd /home/ws_dev
     source /opt/ros/humble/setup.bash
     sudo apt update
-    sudo rosdep update
+    sudo rosdep fix-permissions
+    rosdep update
     sudo rosdep install --from-paths src --ignore-src -y --rosdistro humble
     source /opt/ros/humble/setup.bash
     colcon build --symlink-install
