480e88adc998671a371fa5b71b90056f8833889a
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2024-10-12 15:42:13 -0600

    Moved everything into src, updated stuff

--- .devcontainer/post-create.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. /home/webots_ws/install/setup.bash
-sudo apt update
-rosdep update --rosdistro=humble
-rosdep install --from-paths /home/ws/src --ignore-src -y --rosdistro=humble
-chmod +x /home/webots_ws/install/webots_ros2_driver/lib/webots_ros2_driver/ros2_supervisor.py 
-sudo chown -R deweykai /home/ws/
--- bash-into-docker.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Exit bash by pressing ctrl d
-# Execute by running . bash-into-docker.bash
-
-docker-compose run -w /home/ws_dev/src/haql-rover -it ralphee bash
