6dbd49d1d8847f0f82fdc761ba82bedf2cb9c1ed
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2024-11-05 16:38:28 +0000

    yippie

--- install-zig-maps.bash
+++ install-zig-maps.bash
@@ -9,9 +9,6 @@ if [ ! -d ~/haql-rover/zig/zig_compiler ]; then
     sudo mv zig-linux-x86_64-0.14.0-dev.2063+5ce17ecfa zig_compiler
 fi
 
-~/haql-rover/zig/zig_compiler/zig build
-
-sudo cp ~/haql-rover/zig/zig-out/lib/libzigmaps.so /usr/lib
-sudo cp ~/haql-rover/zig/zig-out/include/zigmaps.h /usr/include
+~/ralphee_ws/zig/zig_compiler/zig build --release=fast -p /usr
 
 cd $currdir
