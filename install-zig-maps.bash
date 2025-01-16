3e2f2f9e638a6fee7a7bacb3105a544462824be3
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-01-16 17:19:57 +0000

    testing stuff

--- install-zig-maps.bash
+++ install-zig-maps.bash
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 currdir=$(pwd)
+arch=$(uname -i)
 
 cd ~/ralphee_ws/zig
 
 if [ ! -d ~/ralphee_ws/zig/zig_compiler ]; then
-    tar -xf ~/ralphee_ws/zig/zig_compiler.tar.xz
-    mv zig-linux-x86_64-0.14.0-dev.2063+5ce17ecfa zig_compiler
+    if [ $arch == x86_64 ]; then
+        tar -xf ~/ralphee_ws/zig/zig-amd64.tar.xz
+        mv zig-linux-x86_64-0.14.0-dev.2063+5ce17ecfa zig_compiler
+    else
+        tar -xf ~/ralphee_ws/zig/zig-arm64.tar.xz
+        mv zig-linux-x86_64-0.14.0-dev.2063+5ce17ecfa zig_compiler
+    fi
 fi
 
 ~/ralphee_ws/zig/zig_compiler/zig build --release=fast -p /usr
