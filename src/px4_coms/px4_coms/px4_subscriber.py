99d0e45d54fc5f26f8dfea74ecd311a8a713563a
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-02-20 21:31:10 +0000

    advertiser now modifies motor speed

--- src/px4_coms/px4_coms/px4_subscriber.py
+++ src/px4_coms/px4_coms/px4_subscriber.py
@@ -4,9 +4,11 @@ from px4_msgs.msg import *
 import rclpy
 from rclpy.node import Node
 
-class Px4Service(Node):
+class Px4Subscriber(Node):
     def __init__(self):
+        pass
+    def send_debug_msg(self):
         pass
 
 def main(args=None):
-    pass
+    print('hello')
