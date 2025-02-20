2d5afd53004d1af979dd1400984978cd1fa2083c
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-02-20 17:47:03 +0000

    made some changes

--- install-deps.bash
+++ install-deps.bash
@@ -5,7 +5,7 @@ sudo apt update
 sudo rosdep fix-permissions
 rosdep update
 sudo rosdep install --from-paths src --ignore-src -y --rosdistro $ROS_DISTRO
-if [$? != 0]; then
+if [ $? != 0 ]; then
     echo rosdep install failed
     return 1
 fi
