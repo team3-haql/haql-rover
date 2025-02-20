14e0e4ae86916d987a6dc8edf3336e270cbdb340
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-02-20 19:02:03 +0000

    px4_coms

---
+++ src/px4_coms/px4_coms/px4_service.py
@@ -0,0 +1,12 @@
+from px4_msgs.srv import VehicleCommand
+from px4_msgs.msg import *
+
+import rclpy
+from rclpy.node import Node
+
+class Px4Service(Node):
+    def __init__(self):
+        pass
+
+def main(args=None):
+    pass
