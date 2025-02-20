8c4de0e83b6c2c98bc407cf7d93d2921b791960f
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-02-20 18:18:52 +0000

    made zig maps work again

--- install-zig-maps.bash
+++ install-zig-maps.bash
@@ -5,21 +5,17 @@ arch=$(uname -i)
 
 cd ~/haql-rover/zig
 
-if [ ! -d ~/ralphee_ws/zig/zig_compiler ]; then
+if [ ! -d ~/haql-rover/zig/zig_compiler ]; then
     if [ $arch == x86_64 ]; then
-        tar -xf ~/ralphee_ws/zig/zig-amd64.tar.xz
+        tar -xf ~/haql-rover/zig/zig-amd64.tar.xz
         mv zig-linux-x86_64-0.14.0-dev.2063+5ce17ecfa zig_compiler
     else
-        tar -xf ~/ralphee_ws/zig/zig-arm64.tar.xz
+        tar -xf ~/haql-rover/zig/zig-arm64.tar.xz
         mv zig-linux-aarch64-0.14.0-dev.2647+5322459a0 zig_compiler
     fi
-if [ ! -d ~/haql-rover/zig/zig_compiler ]; then
-    sudo tar -xf ~/haql-rover/zig/zig.tar.xz
-    sudo mv zig-linux-x86_64-0.14.0-dev.2063+5ce17ecfa zig_compiler
 fi
 
 ~/ralphee_ws/zig/zig_compiler/zig build --release=fast -p /usr
-~/haql-rover/zig/zig_compiler/zig build
 
 sudo cp ~/haql-rover/zig/zig-out/lib/libzigmaps.so /usr/lib
 sudo cp ~/haql-rover/zig/zig-out/include/zigmaps.h /usr/include
